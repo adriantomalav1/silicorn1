1bd38c8c8c6db08583a8d4aaf0dee8392cd7a11c
Adrian Tomala <98851113+adriantomalav@users.noreply.github.com>
2024-01-12 16:00:04 -0500

    fix db port

--- conf/database.py
+++ conf/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:nomelase@silicorn.ctmmjxlkwowe.us-west-2.rds.amazonaws.com:3306/silicorn"
+DATABASE_URL = "mysql+mysqlconnector://root:nomelase@silicorn.ctmmjxlkwowe.us-west-2.rds.amazonaws.com:3306"
 
 engine = create_engine(DATABASE_URL)
 session_maker = sessionmaker(bind=engine)
